88960f56c709c4f084d8227e020d8d045d5a1242
jaszdev <javier.sandoval@ucr.ac.cr>
2020-08-31 14:24:18 -0600

    Cambios de Interfaz a Clase Abstracta

---
+++ proyecto ia/Assets/Scripts/IGenIndividual.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public interface IGenIndividual<T> where T : ICloneable
+{
+    //public List<T> DNA { get; set; }
+    //public int DNALength => DNA.Count;
+    //public double Fitness { get; set; }
+    //public void Mutate(double lowerBound, double upperBound, double chancePerGene);
+    //public IGenIndividual<T> Cross(IGenIndividual<T> other);
+}
+
---
+++ proyecto ia/Assets/Scripts/Individual.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public abstract class Individual<T> 
+{
+    public List<T> DNA { get; set; }
+    public int DNA_Length => DNA.Count;
+    public double Fitness { get; set; }
+    
+    public abstract Individual<T> Cross(Individual<T> other);
+
+    public abstract void Mutate(float lowerBound, float upperBound, float chancePerGene);
+
+}
